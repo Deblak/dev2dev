063f57fbe1565231224410057c478fc96337276d
Deblak <deblak@mailo.com>
2025-04-08 16:08:28 +0200

    init form authenticate or create account

--- dev2dev-presentation/src/views/AccountLogInView.vue
+++ dev2dev-presentation/src/views/AccountLogInView.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Account Login</h1>
+    <main>
+        <form>
+            <div>
+                <label for="username">{{$t('email')}}</label>
+                <input id="username" :placeholder="$t('emailPlaceholder')"></input>
+            </div>
+            <div>
+                <label for="password">{{$t('password')}}</label>
+                <input id="password" type="password" :placeholder="$t('passwordPlaceholder')"></input>
+            </div>
+            <RouterLink to="/create-account">{{$t('signup')}}</RouterLink>
+            <button type="submit">{{$t('login')}}</button>
+        </form>
+    </main>
 </template>
+<style scoped src="../styles/login.css">
+</style>
