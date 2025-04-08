5400bc180111404aea5340815ae343d337d565f6
LFSchefer <lf.schefer@gmail.com>
2025-04-08 23:20:05 +0200

    add vue components

---
+++ dev2dev-presentation/src/components/NotificationBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+    console.log("Notification bar monted")
+})
+</script>
+
+<template>
+    <span class="material-symbols-outlined">notifications</span>
+</template>
+
+<style scoped>
+
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 24
+}
+
+</style>
