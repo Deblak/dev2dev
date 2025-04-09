81ebef2e372fd60a1057ac686dcb372c6b5ee24a
LFSchefer <lf.schefer@gmail.com>
2025-04-09 10:21:56 +0200

    add header

--- dev2dev-presentation/src/components/NotificationBar.vue
+++ dev2dev-presentation/src/components/NotificationBar.vue
@@ -4,20 +4,49 @@ import { onMounted, ref } from 'vue';
 onMounted(() => {
     console.log("Notification bar monted")
 })
+
 </script>
 
 <template>
+  <div class="notification-bell clicable">
     <span class="material-symbols-outlined">notifications</span>
+    <p class="notification-count">0</p>
+    <div class="notification-count-bg"></div>
+  </div>
 </template>
 
 <style scoped>
+
+.clicable {
+  cursor: pointer;
+}
+
+.notification-count-bg {
+  position: absolute;
+  background-color: white;
+  width: 16px;
+  height: 16px;
+  border-radius: 100%;
+  top: -5px;
+  right: -7px;
+}
+
+.notification-count {
+  position: absolute;
+  top: -22px;
+  right: -3px;
+  z-index: 1;
+}
+
+.notification-bell {
+  position: relative;
+}
 
 .material-symbols-outlined {
   font-variation-settings:
   'FILL' 0,
   'wght' 400,
   'GRAD' 0,
-  'opsz' 24
+  'opsz' 40
 }
-
 </style>
