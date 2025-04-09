23c47d3ef74a0991551eda30ef670e070538b84b
LFSchefer <lf.schefer@gmail.com>
2025-04-09 11:42:56 +0200

    add fetchEventSource

--- dev2dev-presentation/src/components/NotificationBar.vue
+++ dev2dev-presentation/src/components/NotificationBar.vue
@@ -1,8 +1,28 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
 
-onMounted(() => {
+const sseConnectionActive = ref(false);
+const notifications = ref([])
+
+onMounted(async () => {
     console.log("Notification bar monted")
+    await fetchEventSource(`${import.meta.env.VITE_API_URL}sse/test`, {
+      async onopen(response) {
+        if (response.ok ) {
+          sseConnectionActive.value = true;
+            return; // everything's good
+        } else if (response.status >= 400 && response.status < 500 && response.status !== 429) {
+            throw new FatalError();
+        } else {
+            throw new RetriableError();
+        }
+    },
+    onmessage(msg) {
+        console.log(msg.data);
+    }
+});
+
 })
 
 </script>
@@ -10,7 +30,7 @@ onMounted(() => {
 <template>
   <div class="notification-bell clicable">
     <span class="material-symbols-outlined">notifications</span>
-    <p class="notification-count">0</p>
+    <p class="notification-count">{{ notifications.length }}</p>
     <div class="notification-count-bg"></div>
   </div>
 </template>
