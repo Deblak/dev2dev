fc9d5dbed911bac4d70db47a38852a7816a44241
Deblak <deblak@mailo.com>
2025-04-10 09:58:11 +0200

    Fix dml database and service for roles

--- dev2dev-data/Scripts/Account.dml.sql
+++ dev2dev-data/Scripts/Account.dml.sql
@@ -3,14 +3,14 @@ DELETE FROM t_accounts;
 INSERT INTO t_accounts (username, password, email_validate, id_role)
 VALUES (
   'riri@mail.com', '$2a$12$S6f2tsM9xQv51P1zwlvSDuVLuGgHj4/T4ez6RTdTbgzpB3FnQAek.', '1', 
-  (SELECT id FROM t_roles WHERE name = 'Developer'));
+  (SELECT id FROM t_roles WHERE name = 'MEMBER'));
 
 INSERT INTO t_accounts (username, password, email_validate, id_role)
 VALUES (
   'fifi@mail.com', '$2a$12$S6f2tsM9xQv51P1zwlvSDuVLuGgHj4/T4ez6RTdTbgzpB3FnQAek.', '1', 
-  (SELECT id FROM t_roles WHERE name = 'Member'));
+  (SELECT id FROM t_roles WHERE name = 'MEMBER'));
 
 INSERT INTO t_accounts (username, password, email_validate, id_role)
 VALUES (
   'loulou@mail.com', '$2a$12$S6f2tsM9xQv51P1zwlvSDuVLuGgHj4/T4ez6RTdTbgzpB3FnQAek.', '1', 
-  (SELECT id FROM t_roles WHERE name = 'Integrator'));
+  (SELECT id FROM t_roles WHERE name = 'INTEGRATOR'));
