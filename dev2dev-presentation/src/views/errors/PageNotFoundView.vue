f9982ee438618d270c9ab2b9ed3fd6648371f957
Saladin88 <145976634+Saladin88@users.noreply.github.com>
2025-05-15 09:30:48 +0200

    Update PageNotFoundView.vue
    optional vue regarding if you have a token or not

--- dev2dev-presentation/src/views/errors/PageNotFoundView.vue
+++ dev2dev-presentation/src/views/errors/PageNotFoundView.vue
@@ -1,5 +1,17 @@
 <template>
     <h1>404 - Page not found</h1>
-    <RouterLink to="/">{{$t('login')}}</RouterLink>
-
+    <div v-if="!token">
+        <RouterLink to="/">{{$t('login')}}</RouterLink>
+    </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+const token = ref(null);
+
+onMounted(() => {
+  token.value = localStorage.getItem("jwtToken");
+});
+</script>
